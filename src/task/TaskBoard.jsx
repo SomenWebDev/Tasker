045d316c61e83ed8aecdf7239b8fe4b3968616d4
SomenWebDev <somensingha18@gmail.com>
2024-01-18 00:37:59 +0600

    ccysf

---
+++ src/task/TaskBoard.jsx
@@ -0,0 +1,100 @@
+import { useState } from "react";
+import SearchTask from "./SearchTask";
+import TaskActions from "./TaskActions";
+import TaskList from "./TaskList";
+import AddTaskModal from "./AddTaskModal";
+
+export default function TaskBoard() {
+  const defaultTask = {
+    " id": crypto.randomUUID(),
+    title: "Learn React",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, dolorum.",
+    tags: ["react", "javascript", "web development"],
+    priority: "High",
+    isFavorite: false,
+  };
+  const [tasks, setTasks] = useState([defaultTask]);
+  const [showAddModal, setShowAddModal] = useState(false);
+  const [taskToUpdate, setTaskToUpdate] = useState(null);
+
+  function handleAddTask(newTask, isAdd) {
+    if (isAdd) {
+      setTasks([...tasks, newTask]);
+    } else {
+      setTasks(
+        tasks.map((task) => {
+          if (task.id === newTask.id) {
+            return newTask;
+          }
+          return task;
+        })
+      );
+    }
+
+    setShowAddModal(false);
+  }
+
+  function handleEditTask(task) {
+    console.log("Edit a click");
+    setTaskToUpdate(task);
+    setShowAddModal(true);
+  }
+
+  function handleCloseClick() {
+    setShowAddModal(false);
+    setTaskToUpdate(null);
+  }
+  function handleDeleteTask(taskId) {
+    const tasksAfterDelete = tasks.filter((task) => task.id !== taskId);
+    setTasks(tasksAfterDelete);
+  }
+
+  function handleDeleteAllClick() {
+    tasks.length = 0;
+    setTasks([...tasks]);
+  }
+  function handleFavorite(taskId) {
+    const taskIndex = tasks.findIndex((task) => task.id === taskId);
+    const newTasks = [...tasks];
+    newTasks[taskIndex].isFavorite = !newTasks[taskIndex].isFavorite;
+    setTasks(newTasks);
+  }
+  function handleSearch(searchTerm) {
+    console.log(searchTerm);
+    const filtered = tasks.filter((task) =>
+      task.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setTasks([...filtered]);
+  }
+
+  return (
+    <section className="mb-20" id="tasks">
+      {showAddModal && (
+        <AddTaskModal
+          onSave={handleAddTask}
+          onCloseClick={handleCloseClick}
+          taskToUpdate={taskToUpdate}
+        />
+      )}
+      <div className="container">
+        <div className="p-2 flex justify-end">
+          <SearchTask onSearch={handleSearch} />
+        </div>
+
+        <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
+          <TaskActions
+            onAddClick={() => setShowAddModal(true)}
+            onDeleteAllClick={() => handleDeleteAllClick()}
+          />
+          <TaskList
+            tasks={tasks}
+            onEdit={handleEditTask}
+            onDelete={handleDeleteTask}
+            onFav={handleFavorite}
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
